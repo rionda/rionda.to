472cee956d7945c651dbb683ed5475eb8caf54fe
Matteo Riondato <matteo@cs.brown.edu>
2015-09-30 01:06:23 +0000

    Use ordered list for publications. While here, remove an annoying indent in main content.

--- pubs.php
+++ pubs.php
@@ -13,83 +13,89 @@
 			of the original publications.</p>
 
 			<h3>Journal Articles</h3>
-			<p>M. Riondato and E. M. Kornaropoulos. <a
-				href="papers/RiondatoKornaropoulos-BetweennessSampling-DMKD.pdf">Fast
-				Approximation of Betweenness Centrality through Sampling</a>, <em>Data
-				Mining and Knowledge Discovery</em>, in press</p>
-			<p>M. Riondato and E. Upfal. <a
-				href="papers/RiondatoUpfal-FrequentItemsetsSampling-TKDD.pdf">Efficient
-				Discovery of Association Rules and Frequent Itemsets through Sampling
-				with Tight Performance Guarantees</a>, <em>ACM Transactions on Knowledge
-			Discovery from Data</em>, 8(4):20, 2014</p>
-			<p>* A. Pietracaprina, M. Riondato, E. Upfal, and F. Vandin. <a
-				href="papers/PietracaprinaEtAl-TopKItemsetsProgressiveSampling-DMKD.pdf">Mining
-				Top-k Frequent Itemsets through Progressive Sampling</a>, <em>Data
-				Mining and Knowledge Discovery</em>, 21(2):310–326, 2010</p>
+			<ol>
+				<li>M. Riondato and E. M. Kornaropoulos. <a
+					href="papers/RiondatoKornaropoulos-BetweennessSampling-DMKD.pdf">Fast
+					Approximation of Betweenness Centrality through Sampling</a>, <em>Data
+					Mining and Knowledge Discovery</em>, in press</li>
+				<li>M. Riondato and E. Upfal. <a
+					href="papers/RiondatoUpfal-FrequentItemsetsSampling-TKDD.pdf">Efficient
+					Discovery of Association Rules and Frequent Itemsets through Sampling
+					with Tight Performance Guarantees</a>, <em>ACM Transactions on Knowledge
+				Discovery from Data</em>, 8(4):20, 2014</li>
+				<li>* A. Pietracaprina, M. Riondato, E. Upfal, and F. Vandin. <a
+					href="papers/PietracaprinaEtAl-TopKItemsetsProgressiveSampling-DMKD.pdf">Mining
+					Top-k Frequent Itemsets through Progressive Sampling</a>, <em>Data
+					Mining and Knowledge Discovery</em>, 21(2):310–326, 2010</li>
+			</ol>
 			<h3>Conference Papers</h3>
-			<p>M. Riondato and E. Upfal. <a
-				href="papers/RiondatoUpfal-FrequentItemsetsSamplingRademacher-KDD.pdf">Mining
-				Frequent Itemsets through Progressive Sampling with Rademacher
-				Averages</a>, <em>ACM KDD'15</em>, 2015</p>
-			<p>* A. Anagnastopoulos, L. Becchetti, A. Fazzone, I. Mele, and M.
-			Riondato. <a
-				href="papers/AnagnostopoulosEtAl-ExpertsMaxFindingCrowdsourcing.pdf">The
-				Importance of Being Experts: Efficient Max-Finding in Crowdsourcing</a>,
-			<em>ACM SIGMOD'15</em>, 2015</p>
-			<p>M. Riondato, D. Garc&iacute;a-Soriano, and F. Bonchi. <a
-				href="papers/RiondatoEtAl-GraphSummarization-ICDM.pdf">Graph
-				Summarization with Quality Guarantees</a>, <em>IEEE ICDM'14</em>,
-			2014</p>
-			<p>M. Riondato. <a
-				href="papers/Riondato-SamplingDataMining-ECMLPKDD.pdf">Sampling-based
-				Data Mining Algorithms: Modern Techniques and Case Studies</a>,
-			<em>ECML PKDD'14</em>, 2014</p>
-			<p>M. Riondato and F. Vandin. <a
-				href="papers/RiondatoVandin-TrueFrequentItemsets.pdf">Finding the True
-				Frequent Itemsets</a> <em>SIAM SDM'14</em>, 2014</p>
-			<p>M. Riondato and E. M. Kornaropoulos. <a
-			href="papers/RiondatoKornaropoulos-BetweennessSampling-DMKD.pdf">Fast
-			Approximation of Betweenness Centrality through Sampling</a>, <em>ACM
-			WSDM'14</em>, 2014</p>
-			<p>M. Riondato, J. A. DeBrabant, R. Fonseca, and E. Upfal. <a
-			href="papers/RiondatoEtAl-PARMA-CIKM.pdf">PARMA: A Parallel Randomized
-			Algorithm for Association Rules Mining in MapReduce</a>, <em>ACM
-			CIKM'12</em>, 2012</p>
-			<p>M. Riondato and E. Upfal. <a
-				href="papers/RiondatoUpdal-FrequentItemsetsSampling-TKDD.pdf">Efficient
-				Discovery of Association Rules and Frequent Itemsets through Sampling
-				with Tight Performance Guarantees</a>, <em>ECML PKDD'12</em>, 2012</p>
-			<p>* A. Pietracaprina, G. Pucci, M. Riondato, F. Silvestri, and E. Upfal.
-			<a
-				href="papers/PietracaprinaEtAl-SpaceRoundMapReduce-ICS.pdf">Space-round
-				Tradeoffs for MapReduce Computations</a>, <em>ACM ICS'12</em>, 2012</p>
-			<p>M. Akdere, U. Çetintemel, M. Riondato, E. Upfal, and S. B. Zdonik.
-			<a
-				href="papers/AkdereEtAl-LearningQueryPerformance-ICDE.pdf">Learning-based
-				Query Performance Modeling and Prediction</a>, <em>IEEE
-			ICDE'12</em>, 2012</p>
-			<p>M. Riondato, M. Akdere, U. Çetintemel, S. B. Zdonik, and E. Upfal. <a
-				href="papers/RiondatoEtAl-VCDimensionSQLQueries-ECMLPKDD.pdf">The
-				VC-dimension of SQL Queries and Selectivity Estimation through
-			Sampling</a>, <em>ECML PKDD'11</em>, 2011</p>
-			<p>M. Akdere, U. Çetintemel, M. Riondato, E. Upfal, and S. B. Zdonik.
-			<a href="papers/AkdereEtAl-CasePredictiveDatabase-CIDR.pdf">The Case for
-			Predictive Database Systems: Opportunities and Challenges</a>,
-			<em>CIDR'11</em>, 2011</p>
+			<ol start=4>
+				<li>M. Riondato and E. Upfal. <a
+					href="papers/RiondatoUpfal-FrequentItemsetsSamplingRademacher-KDD.pdf">Mining
+					Frequent Itemsets through Progressive Sampling with Rademacher
+					Averages</a>, <em>ACM KDD'15</em>, 2015</li>
+				<li>* A. Anagnastopoulos, L. Becchetti, A. Fazzone, I. Mele, and M.
+				Riondato. <a
+					href="papers/AnagnostopoulosEtAl-ExpertsMaxFindingCrowdsourcing.pdf">The
+					Importance of Being Experts: Efficient Max-Finding in Crowdsourcing</a>,
+				<em>ACM SIGMOD'15</em>, 2015</li>
+				<li>M. Riondato, D. Garc&iacute;a-Soriano, and F. Bonchi. <a
+					href="papers/RiondatoEtAl-GraphSummarization-ICDM.pdf">Graph
+					Summarization with Quality Guarantees</a>, <em>IEEE ICDM'14</em>,
+				2014</li>
+				<li>M. Riondato. <a
+					href="papers/Riondato-SamplingDataMining-ECMLPKDD.pdf">Sampling-based
+					Data Mining Algorithms: Modern Techniques and Case Studies</a>,
+				<em>ECML PKDD'14</em>, 2014</li>
+				<li>M. Riondato and F. Vandin. <a
+					href="papers/RiondatoVandin-TrueFrequentItemsets.pdf">Finding the True
+					Frequent Itemsets</a> <em>SIAM SDM'14</em>, 2014</li>
+				<li>M. Riondato and E. M. Kornaropoulos. <a
+				href="papers/RiondatoKornaropoulos-BetweennessSampling-DMKD.pdf">Fast
+				Approximation of Betweenness Centrality through Sampling</a>, <em>ACM
+				WSDM'14</em>, 2014</li>
+				<li>M. Riondato, J. A. DeBrabant, R. Fonseca, and E. Upfal. <a
+				href="papers/RiondatoEtAl-PARMA-CIKM.pdf">PARMA: A Parallel Randomized
+				Algorithm for Association Rules Mining in MapReduce</a>, <em>ACM
+				CIKM'12</em>, 2012</li>
+				<li>M. Riondato and E. Upfal. <a
+					href="papers/RiondatoUpdal-FrequentItemsetsSampling-TKDD.pdf">Efficient
+					Discovery of Association Rules and Frequent Itemsets through Sampling
+					with Tight Performance Guarantees</a>, <em>ECML PKDD'12</em>, 2012</li>
+				<li>* A. Pietracaprina, G. Pucci, M. Riondato, F. Silvestri, and E. Upfal.
+				<a
+					href="papers/PietracaprinaEtAl-SpaceRoundMapReduce-ICS.pdf">Space-round
+					Tradeoffs for MapReduce Computations</a>, <em>ACM ICS'12</em>, 2012</li>
+				<li>M. Akdere, U. Çetintemel, M. Riondato, E. Upfal, and S. B. Zdonik.
+				<a
+					href="papers/AkdereEtAl-LearningQueryPerformance-ICDE.pdf">Learning-based
+					Query Performance Modeling and Prediction</a>, <em>IEEE
+				ICDE'12</em>, 2012</li>
+				<li>M. Riondato, M. Akdere, U. Çetintemel, S. B. Zdonik, and E. Upfal. <a
+					href="papers/RiondatoEtAl-VCDimensionSQLQueries-ECMLPKDD.pdf">The
+					VC-dimension of SQL Queries and Selectivity Estimation through
+				Sampling</a>, <em>ECML PKDD'11</em>, 2011</li>
+				<li>M. Akdere, U. Çetintemel, M. Riondato, E. Upfal, and S. B. Zdonik.
+				<a href="papers/AkdereEtAl-CasePredictiveDatabase-CIDR.pdf">The Case for
+				Predictive Database Systems: Opportunities and Challenges</a>,
+				<em>CIDR'11</em>, 2011</li>
+			</ol>
 
 			<h3>Tutorials</h3>
-			<p>M. Riondato and E. Upfal. <a
-				href="http://bigdata.cs.brown.edu/vctutorial/">VC-Dimension and
-				Rademacher Averages: From Statistical Learning Theory to Sampling
-			Algorithms</a>, <em>ACM KDD'15, ECML PKDD'15, ACM CIKM'15</em></p>
+			<ol start=16>
+				<li>M. Riondato and E. Upfal. <a
+					href="http://bigdata.cs.brown.edu/vctutorial/">VC-Dimension and
+					Rademacher Averages: From Statistical Learning Theory to Sampling
+				Algorithms</a>, <em>ACM KDD'15, ECML PKDD'15, ACM CIKM'15</em></li>
+			</ol>
 
 			<h3>Other technical writings</h3>
-			<p>M. Riondato, <a
+			<li>M. Riondato, <a
 					href="https://www.freebsd.org/doc/en_US.ISO8859-1/books/handbook/jails.html">Jails</a>,
-			in <a href="http://www.freebsd.org/handbook">FreeBSD Handbook</a>.</p>
+			in <a href="http://www.freebsd.org/handbook">FreeBSD Handbook</a>.</li>
 
-			<p >My <strong>coauthors</strong>: <a
-				href="http://www.cs.brown.edu/~makdere/">Mert Akdere</a>, <a
+			<h3>Coauthors</h3>
+			<li><a href="http://www.cs.brown.edu/~makdere/">Mert Akdere</a>, <a
 				href="http://www/aris.me/">Aris Anagnostopoulos</a>, <a
 				href="http://www.dis.uniroma1.it/~becchett/">Luca Becchetti</a>, <a
 				href="http://www.francescobonchi.com">Francesco Bonchi</a>, <a
@@ -105,5 +111,5 @@
 				Pucci</a>, <a href="http://www.dei.unipd.it/~silvest1">Francesco
 				Silvestri</a>, <a href="http://www.cs.brown.edu/~eli">Eli Upfal</a>, <a
 				href="http://www.imada.sdu.dk/~vandinfa/">Fabio Vandin</a>, <a
-				href="http://www.cs.brown.edu/~sbz/">Stan B. Zdonik</a></p>
+				href="http://www.cs.brown.edu/~sbz/">Stan B. Zdonik</a></li>
 <?php include("footer.html"); ?>
