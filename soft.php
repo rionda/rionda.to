4234a991d58ee44b2ebaee00ad2f65f2984be367
Matteo Riondato <matteo@cs.brown.edu>
2015-09-30 21:15:12 +0000

    Edit software page a bit.

--- soft.php
+++ soft.php
@@ -5,18 +5,15 @@
 			href="http://github.com/rionda">GitHub</a>.<br />
 		I also contribute to <a
 		href="https://networkit.iti.kit.edu/">NetworKit</a> as a committer.</p>
-
-		<p><a href="radeprogrfi.tar.bz2">RadeProgrFI</a>: Mining Frequent Itemsets
-		through Progressive Sampling with Rademacher Averages</p>
-		<p><a href="centrsampl.tar.bz2">CentrSampl</a>: Fast Betweenness Centrality
-		Estimation.</p>
-		<p><a href="parma.tar.bz2">PARMA</a>: Frequent Itemsets and Association
-		Rule Mining for MapReduce (<a
+		<p><a href="software/radeprogrfi.tar.bz2">RadeProgrFI</a>: Mining Frequent
+		Itemsets through Progressive Sampling with Rademacher Averages</p>
+		<p><a href="software/parma.tar.bz2">PARMA</a>: Frequent Itemsets and
+		Association Rule Mining for MapReduce (<a
 		href="papers/RiondatoEtAl-PARMA.pdf">Paper</a>). PARMA is also used
 		inside <a
 			href="https://github.com/yahoo/samoa/wiki/Distributed-Stream-Frequent-Itemset-Mining">SAMOA</a>,
 		Yahoo's platform for Big Data streams analysis.</p>
 		<p>In my salad days, I was a <a
 		href="http://www.FreeBSD.org">FreeBSD</a> committer, and the release
-		engineer for <a href="http://www.freesbie.org">FreeSBIE</a>.</p>
+		engineer for <a href="http://www.freesbie.org">FreeSBIE 2.x</a>.</p>
 <?php include("footer.html"); ?>
