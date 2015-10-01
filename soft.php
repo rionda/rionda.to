e73d0b5d15b263a1fe2339f7618039cae962460b
Matteo Riondato <matteo@cs.brown.edu>
2015-10-01 03:23:53 +0000

    Reorganize software page a bit

--- soft.php
+++ soft.php
@@ -2,17 +2,26 @@
 <!-- vim: set ts=2 sw=2 tw=79: -->
 		<h2>Software</h2>
 		<p>My current projects are hosted on <a
-			href="http://github.com/rionda">GitHub</a>.<br />
-		I also contribute to <a
-		href="https://networkit.iti.kit.edu/">NetworKit</a> as a committer.</p>
+			href="https://github.com/rionda?tab=repositories">GitHub</a>.<br />
+		I am also a committer for <a
+		href="https://networkit.iti.kit.edu/">NetworKit</a>, a toolkit for
+		high-performance network analysis.</p> 
+
+		<p><a href="https://github.com/rionda/chato-notes">ChaTo-notes</a>: a
+		LaTeX package to insert notes, todo lists, questions, and answers. It is
+		useful when writing papers with multiple authors.</p>
 		<p><a href="software/radeprogrfi.tar.bz2">RadeProgrFI</a>: Mining Frequent
 		Itemsets through Progressive Sampling with Rademacher Averages</p>
-		<p><a href="software/parma.tar.bz2">PARMA</a>: Frequent Itemsets and
-		Association Rule Mining for MapReduce (<a
-		href="papers/RiondatoEtAl-PARMA.pdf">Paper</a>). PARMA is also used
-		inside <a
-			href="https://github.com/yahoo/samoa/wiki/Distributed-Stream-Frequent-Itemset-Mining">SAMOA</a>,
-		Yahoo's platform for Big Data streams analysis.</p>
+		<p>Centrsampl: If you are looking for the software to approximate
+		betweenness centrality via sampling, check out the implementation in <a
+		href="https://networkit.iti.kit.edu/">NetworKit</a> as
+		<em>ApproxBetweenness</em>.</p>
+		<p><a href="software/parma.tar.bz2">PARMA</a> (only for archival purposes):
+		Frequent Itemsets and Association Rule Mining for MapReduce (<a
+		href="papers/RiondatoEtAl-PARMA.pdf">Paper</a>).<br />
+		PARMA is also used inside <a
+		href="https://samoa.incubator.apache.org">Apache SAMOA</a>, Yahoo's
+		platform for Big Data streams analysis.</p>
 		<p>In my salad days, I was a <a
 		href="http://www.FreeBSD.org">FreeBSD</a> committer, and the release
 		engineer for <a href="http://www.freesbie.org">FreeSBIE 2.x</a>.</p>
