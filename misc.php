a8315e07bee005a7e69681500bc7d158adc072bc
Matteo Riondato <matteo@cs.brown.edu>
2015-09-30 23:54:18 -0400

    Move files to files/ directory, and change html accordingly. Add mp3 with name.

--- misc.php
+++ misc.php
@@ -1,9 +1,9 @@
 <?php include("header.html"); ?>
 <!-- vim: set ts=2 sw=2 tw=79: -->
 		<h2>Misc</h2>
-		<p><a href="name.au">How to pronounce my name</a> (muh-TAY-oh
-		REE-awn-DAH-toh)</p>
-		<p><a href="riondapgp.asc">My GnuPG/PGP key</a> (Key fingerprint = F0F3
+		<p><a href="files/matteo_riondato.mp3">How to pronounce my name</a>
+		(muh-TAY-oh REE-awn-DAH-toh)</p>
+		<p><a href="files/riondapgp.asc">My GnuPG/PGP key</a> (Key fingerprint = F0F3
 1B43 035D 65B1 08E9  4D66 D8CA 78A5 1EC5 6BEC)</p>
 		<p><a href="http://www.riondato.com">Silvio Riondato</a>, my father</p>
 		<p><a href="http://www.ezioriondato.org">Ezio
