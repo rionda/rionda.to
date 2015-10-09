0941de0680082f4863601b01c30779dd2f6370dd
Matteo Riondato <matteo@cs.brown.edu>
2015-10-09 14:47:32 +0000

    Add title of monash talk and dates of CIKM and monash. Also fix typo.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include("header.html"); ?>
 				<!-- vim: set ts=2 sw=2 tw=79: -->
 				<p>I am a researcher in computer science, currently working in the
-				Labs group at <a href="http://www.twosigma.com/">Two Sigma</a>, an highly
+				Labs group at <a href="http://www.twosigma.com/">Two Sigma</a>, a highly
 				innovative investment firm in New York City.</p>
 				<p>My research interest is in algorithmic data analysis. I develop
 				theory and methods to extract the most information from large datasets,
@@ -17,12 +17,13 @@
 				Laplace (10th).</p>
 				<h2>News</h2>
 				<ul>
-					<li><strong>CIKM'15:</strong> I'm presenting our <a
+					<li><strong>CIKM'15:</strong> On Friday 10/23, I'm presenting a <a
 					href="http://bigdata.cs.brown.edu/vctutorial">tutorial on
 					VC-Dimension and Rademacher Averages for sampling
 					algorithms</a>.</li>
-					<li><strong>Monash University:</strong> I'm giving a talk on
-						statistical learning theory and data mining.</li>
+					<li><strong>Monash University:</strong> On Wednesday 10/28 I'm giving
+					a talk: <em>Travel pictures from another world: Statistical Learning
+					Theory meets Data Mining</em>.</li>
 					<li><strong>WWW'16:</strong> I'm a PC member for the Social Networks
 						and Graph Analysis area.</li>
 					<li><strong>Brown University:</strong> In Spring 2016, I will be
