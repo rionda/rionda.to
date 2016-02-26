fc66abfd5cc890fc0388fb815a4f7e597f86f973
Matteo Riondato <riondato@acm.org>
2016-02-25 21:39:36 -0800

    Add links for the cv and deploywebsite script.

---
+++ deploywebsite.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+UPLOADHOST=rionda.to
+UPLOADDIR=rionda.to
+
+rsync --rsh=ssh --verbose --compress --recursive --copy-links --perms --no-o --no-g --chmod=F664 --chmod=D2775 --exclude old --exclude cv --exclude='.git**' --exclude deploywebsite.sh . ${UPLOADHOST}:${UPLOADDIR}
