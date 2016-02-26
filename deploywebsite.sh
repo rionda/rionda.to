f6703978f57dff59c01f5919771369adefbff760
Matteo Riondato <riondato@acm.org>
2016-02-25 21:42:12 -0800

    Add script to update the submodules.

--- deploywebsite.sh
+++ deploywebsite.sh
@@ -3,4 +3,8 @@
 UPLOADHOST=rionda.to
 UPLOADDIR=rionda.to
 
-rsync --rsh=ssh --verbose --compress --recursive --copy-links --perms --no-o --no-g --chmod=F664 --chmod=D2775 --exclude old --exclude cv --exclude='.git**' --exclude deploywebsite.sh . ${UPLOADHOST}:${UPLOADDIR}
+rsync --rsh=ssh --verbose --compress --recursive --copy-links \
+    --perms --no-o --no-g --chmod=F664 --chmod=D2775 \
+    --exclude old --exclude cv --exclude='.git**' \
+    --exclude deploywebsite.sh --exclude update_submodules.sh \
+    . ${UPLOADHOST}:${UPLOADDIR}
---
+++ update_submodules.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git submodule foreach git pull origin master
+git submodule update --init --recursive
