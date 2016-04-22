92529b4672c9c5622b01637f9683a947d464cf48
Matteo Riondato <riondato@acm.org>
2016-04-22 11:57:54 -0400

    Add The computer journal.

--- serv.php
+++ serv.php
@@ -2,8 +2,8 @@
 <!-- vim: set ts=2 sw=2 tw=79: -->
 		<h2>Service</h2>
 		<h3>Program Committees</h3>
-		<p class="notop">Grace Hopper'16, ECML PKDD'16, ACM KDD'16, WWW'16, ACM
-		KDD'15, ACM CIKM'15, ACM CIKM'14</p>
+		<p class="notop">ACM CIKM'16, Grace Hopper'16, ECML PKDD'16, ACM KDD'16,
+		WWW'16, ACM KDD'15, ACM CIKM'15, ACM CIKM'14</p>
 		<h3>Journal Reviewing</h3>
 		<p class="notop"><em>Data Mining and Knowledge Discovery</em> (DMKD/DAMI),
 		<em>Engineering Applications of Artificial Intelligence</em> (EAAI),
@@ -11,12 +11,13 @@
 		Knowledge and Data Engineering</em> (TKDE), <em>IEEE Transactions on Network
 		Science and Engineering</em> (TNSE), <em>IEEE Transactions on Parallel and
 		Distributed Systems</em> (TPDS), <em>IEEE Transactions on Services
-		Computing</em> (TSC), <em>VLDB Journal</em> (VLDBJ)</p>
+		Computing</em> (TSC), <em>The Computer Journal</em>, <em>VLDB Journal</em>
+		(VLDBJ)</p>
 		<h3>Conference Reviewing</h3>
-		<p class="notop">ECML PKDD'16, ACM KDD'16, WWW'16, ACM WSDM'16, ACM
-		CIKM'15, ACM KDD'15, ISAAC'15, SIAM SDM'15, IEEE ICDE'15, ACM CIKM'14,
-		DISC'14, ACM WSDM'14, WWW'14, ICALP'14, IEEE BigData'13, MFCS'13, ACM
-		WSDM'13, IEEE IPDPS'12, ACM ICS'12, RANDOM'11</p>
+		<p class="notop">ACM CIKM'16, Grace Hopper'16, ECML PKDD'16, ACM KDD'16,
+		WWW'16, ACM WSDM'16, ACM CIKM'15, ACM KDD'15, ISAAC'15, SIAM SDM'15, IEEE
+		ICDE'15, ACM CIKM'14, DISC'14, ACM WSDM'14, WWW'14, ICALP'14, IEEE
+		BigData'13, MFCS'13, ACM WSDM'13, IEEE IPDPS'12, ACM ICS'12, RANDOM'11</p>
 		<h3>Conference Session Chairing</h3>
 		<p class="notop">Social and Graphs 4 at ACM KDD'15</p>
 		<h3>Grant Proposal Reviewing</h3>
