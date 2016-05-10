e397db2e36014dc4bdd2c8775c67c7fb934e3343
Matteo Riondato <riondato@acm.org>
2016-05-10 12:03:29 -0400

    Reorganize service page.

--- serv.php
+++ serv.php
@@ -2,23 +2,32 @@
 <!-- vim: set ts=2 sw=2 tw=79: -->
 		<h2>Service</h2>
 		<h3>Program Committees</h3>
-		<p class="notop">ACM CIKM'16, Grace Hopper'16, ECML PKDD'16, ACM KDD'16,
-		WWW'16, ACM KDD'15, ACM CIKM'15, ACM CIKM'14</p>
+		<p class="notop">2016: ACM CIKM'16, Grace Hopper'16, ECML PKDD'16, ACM
+			KDD'16, WWW'16</p>
+		<p class="notop">2015: ACM KDD'15, ACM CIKM'15</p>
+		<p class="notop">2014: ACM CIKM'14</p>
 		<h3>Journal Reviewing</h3>
-		<p class="notop"><em>Data Mining and Knowledge Discovery</em> (DMKD/DAMI),
-		<em>Engineering Applications of Artificial Intelligence</em> (EAAI),
-		<em>Knowledge and Information Systems</em> (KAIS), <em>IEEE Transactions on
-		Knowledge and Data Engineering</em> (TKDE), <em>IEEE Transactions on Network
-		Science and Engineering</em> (TNSE), <em>IEEE Transactions on Parallel and
-		Distributed Systems</em> (TPDS), <em>IEEE Transactions on Services
-		Computing</em> (TSC), <em>The Computer Journal</em>, <em>VLDB Journal</em>
-		(VLDBJ)</p>
+		<p class="notop"><em>Data Mining and Knowledge Discovery</em> (DMKD/DAMI)</p>
+		<p class="notop"><em>Engineering Applications of Artificial Intelligence</em> (EAAI)</p>
+		<p class="notop"><em>Knowledge and Information Systems</em> (KAIS)</p>
+		<p class="notop"><em>IEEE Transactions on Knowledge and Data Engineering</em> (TKDE)</p>
+		<p class="notop"><em>IEEE Transactions on Network Science and Engineering</em> (TNSE)</p>
+		<p class="notop"><em>IEEE Transactions on Parallel and Distributed Systems</em> (TPDS)</p>
+		<p class="notop"><em>IEEE Transactions on Services Computing</em> (TSC)</p>
+		<p class="notop"><em>The Computer Journal</em></p>
+		<p class="notop"><em>VLDB Journal</em> (VLDBJ)</p>
 		<h3>Conference Reviewing</h3>
-		<p class="notop">ACM CIKM'16, Grace Hopper'16, ECML PKDD'16, ACM KDD'16,
-		WWW'16, ACM WSDM'16, ACM CIKM'15, ACM KDD'15, ISAAC'15, SIAM SDM'15, IEEE
-		ICDE'15, ACM CIKM'14, DISC'14, ACM WSDM'14, WWW'14, ICALP'14, IEEE
-		BigData'13, MFCS'13, ACM WSDM'13, IEEE IPDPS'12, ACM ICS'12, RANDOM'11</p>
+		<p class="notop">2016: ACM CIKM'16, Grace Hopper'16, ECML PKDD'16, ACM
+			KDD'16, WWW'16, ACM WSDM'16</p>
+		<p class="notop">2015: ACM CIKM'15, ACM KDD'15, ISAAC'15, SIAM SDM'15, IEEE
+			ICDE'15</p>
+		<p class="notop">2014: ACM CIKM'14, DISC'14, ACM WSDM'14, WWW'14,
+			ICALP'14</p>
+		<p class="notop">2013: IEEE BigData'13, MFCS'13, ACM WSDM'13</p>
+		<p class="notop">2012: IEEE IPDPS'12, ACM ICS'12</p>
+		<p class="notop">2011: APPROX-RANDOM'11</p>
 		<h3>Conference Session Chairing</h3>
+		<p class="notop">Social Networks and Graph Analysis 1 at WWW'16</p>
 		<p class="notop">Social and Graphs 4 at ACM KDD'15</p>
 		<h3>Grant Proposal Reviewing</h3>
 		<p class="notop">Sigma Delta Epsilon (Graduate Women in Science) National
