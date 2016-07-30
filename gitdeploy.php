84b3f4acd2e828c5bdf2332db1b1f8959309fbe2
Matteo Riondato <riondato@acm.org>
2016-07-30 12:18:53 -0400

    Deploy webhooks php script.

---
+++ gitdeploy.php
@@ -0,0 +1,52 @@
+<?php
+    /**
+     * GIT DEPLOYMENT SCRIPT
+     *
+     * Used for automatically deploying websites via github securely, more deets here:
+     *
+     *      https://gist.github.com/limzykenneth/baef1b190c68970d50e1
+     */
+
+    // The header information which will be verified
+    $agent=$_SERVER['HTTP_USER_AGENT'];
+    $signature=$_SERVER['HTTP_X_HUB_SIGNATURE'];
+    $body=@file_get_contents('php://input');
+
+    // The commands
+    $commands = array(
+        'git pull origin master',
+        'git submodule sync',
+        'git submodule update'
+    );
+
+    base64_encode($agent);
+    base64_encode($signature);
+
+    if (strpos($agent,'GitHub-Hookshot') !== false){
+        if (hash_equals($signature, verify_request())){
+            // Run the commands
+            foreach($commands AS $command){
+                // Run it
+                $tmp = shell_exec($command);
+            }
+            echo "Deploy successful.";
+        }else{
+            header('HTTP/1.1 403 Forbidden');
+            echo "Invalid request 1.";
+        }
+    }else{
+        header('HTTP/1.1 403 Forbidden');
+        echo "Invalid request 2.";
+    }
+
+    // Generate the hash verification with the request body and the key stored in your .htaccess file
+    function verify_request(){
+        $message = $GLOBALS['body'];
+        $key     = $_ENV['GIT_TOKEN'];
+            $hash    = hash_hmac("sha1", $message, $key);
+            $hash = "sha1=".$hash;
+            return $hash;
+    }
+
+
+?>
