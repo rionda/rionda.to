4886bac215d96ecb2cbab977cd363e1f4c3051aa
Matteo Riondato <riondato@acm.org>
2016-08-19 11:05:19 -0400

    Reindent using whitespaces.

--- gitdeploy.php
+++ gitdeploy.php
@@ -24,25 +24,25 @@
 
     if (strpos($agent,'GitHub-Hookshot') !== false){
         if (hash_equals($signature, verify_request())){
-			if (ini_get('safe_mode')){
-				header('HTTP/1.1 501 Not Implemented');
-				echo "PHP safe mode is enabled, so we cannot exec().";
-			}else{
-				// Run the commands
-				foreach($commands AS $command){
-					$output = array();
-					$return = 0;
-					exec("$command 2>&1", $output, $return);
-					if ($return !== 0){
-						header('HTTP/1.1 500 Internal Server Error');
-						echo "ERROR: Command '$command' exited with status $return:\n";
-					}
-					foreach($output as $line){
-						echo "$line\n";
-					}
-				}
-				echo "Deploy successful.";
-			}
+            if (ini_get('safe_mode')){
+                header('HTTP/1.1 501 Not Implemented');
+                echo "PHP safe mode is enabled, so we cannot exec().";
+            }else{
+                // Run the commands
+                foreach($commands AS $command){
+                    $output = array();
+                    $return = 0;
+                    exec("$command 2>&1", $output, $return);
+                    if ($return !== 0){
+                        header('HTTP/1.1 500 Internal Server Error');
+                        echo "ERROR: Command '$command' exited with status $return:\n";
+                    }
+                    foreach($output as $line){
+                        echo "$line\n";
+                    }
+                }
+                echo "Deploy successful.";
+            }
         }else{
             header('HTTP/1.1 403 Forbidden');
             echo "Invalid request 1.";
