2b5b670b2635f3daa4197e3e28bc41865712024f
Matteo Riondato <riondato@acm.org>
2016-08-23 08:44:05 -0400

    Add ESWA reviewing, remove duplicate line, and fix italics.

--- serv.php
+++ serv.php
@@ -11,18 +11,18 @@
 		<p class="notop"><em>ACM Transactions on Information Systems</em> (TOIS)</p>
 		<p class="notop"><em>ACM Transactions on Knowledge Discovery from Data</em> (TKDD)</p>
 		<p class="notop"><em>Data Mining and Knowledge Discovery</em> (DMKD/DAMI)</p>
-		<p class="notop"><em>Data Mining and Knowledge Discovery</em> (DMKD/DAMI)</p>
 		<p class="notop"><em>Engineering Applications of Artificial Intelligence</em> (EAAI)</p>
+		<p class="notop"><em>Expert Systems with Applications</em> (ESWA)</p>
 		<p class="notop"><em>Knowledge and Information Systems</em> (KAIS)</p>
 		<p class="notop"><em>IEEE Transactions on Knowledge and Data Engineering</em> (TKDE)</p>
 		<p class="notop"><em>IEEE Transactions on Network Science and Engineering</em> (TNSE)</p>
 		<p class="notop"><em>IEEE Transactions on Parallel and Distributed Systems</em> (TPDS)</p>
 		<p class="notop"><em>IEEE Transactions on Services Computing</em> (TSC)</p>
-		<p class="notop"><em>Machine Learning (Mach. Learn.)</em></p>
+		<p class="notop"><em>Machine Learning</em> (Mach. Learn.)</p>
 		<p class="notop"><em>The Computer Journal</em></p>
 		<p class="notop"><em>VLDB Journal</em> (VLDBJ)</p>
 		<h3>Conference Reviewing</h3>
-		<p class="notop">2017: SODA'17</p>
+		<p class="notop">2017: ACM WSDM'17, SODA'17</p>
 		<p class="notop">2016: IEEE ICDM'16, ACM CIKM'16, Grace Hopper'16, ECML
 			PKDD'16, ACM KDD'16, WWW'16, ACM WSDM'16</p>
 		<p class="notop">2015: ACM CIKM'15, ACM KDD'15, ISAAC'15, SIAM SDM'15, IEEE
