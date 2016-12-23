f31553dffd6e06590a296d28b3dcbceb062d1ea3
Matteo Riondato <riondato@acm.org>
2016-12-22 22:02:11 -0500

    Add KDD'17 SPC.

--- serv.php
+++ serv.php
@@ -2,7 +2,8 @@
 <!-- vim: set ts=2 sw=2 tw=79: -->
 		<h2>Service</h2>
 		<h3>Program Committees</h3>
-		<p class="notop">2017: ACM WSDM'17, WWW'17, IEEE ICDE'17</p>
+		<p class="notop">2017: ACM KDD'17 (Senior PC), ACM WSDM'17, WWW'17, IEEE
+			ICDE'17</p>
 		<p class="notop">2016: IEEE ICDM'16, ACM CIKM'16, Grace Hopper'16, ECML
 			PKDD'16, ACM KDD'16, WWW'16</p>
 		<p class="notop">2015: ACM KDD'15, ACM CIKM'15</p>
