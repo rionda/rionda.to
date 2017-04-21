0f9db2b8cc3cbac4aa16e8e7ce7b274f2488ce15
Matteo Riondato <riondato@acm.org>
2017-04-21 14:36:25 -0400

    Fix typo.

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ develop
 					and IEEE ICDE'18. Looking forward to reviewing more works at the
 					intersection of data management and data analytics.</li>
 					<li><strong>Dagstuhl</strong>: I am giving a talk on Rademacher
-					Averages and <a href="papers/RiondatoUpfal-ABRA-ext.pdf">ABRA"</a> at
+					Averages and <a href="papers/RiondatoUpfal-ABRA-ext.pdf">ABRA</a> at
 					the <a
 					href="http://www.dagstuhl.de/en/program/calendar/semhp/?semnr=17141">Probabilistic
 					Methods in the Design and Analysis of Algorithms</a> at Schloss
