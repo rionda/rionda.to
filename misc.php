26f63e037a428c034427ab4c4487b2abb79d6d3c
Matteo Riondato <riondato@acm.org>
2017-07-12 09:00:41 -0400

    Update the fingerprint of my GPG key.

--- misc.php
+++ misc.php
@@ -3,8 +3,8 @@
 		<h2>Misc</h2>
 		<p><a href="files/matteo_riondato.mp3">How to pronounce my name</a>
 		(muh-TAY-oh REE-awn-DAH-toh)</p>
-		<p><a href="files/riondapgp.asc">My GnuPG/PGP key</a> (Key fingerprint: F0F3
-1B43 035D 65B1 08E9  4D66 D8CA 78A5 1EC5 6BEC)</p>
+		<p><a href="files/riondapgp.asc">My GnuPG/PGP key</a> (Key fingerprint:
+			6BDB 8A64 BD21 3F81 3C36  5E6F 1B02 FD49 5425 5501)</p>
 		<p><a href="http://www.riondato.com">Silvio Riondato</a>, my father</p>
 		<p><a href="http://www.ezioriondato.org">Ezio
 			Riondato</a>, my grandfather, also on Wikipedia: <a
