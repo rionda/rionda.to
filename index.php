e48f049fae90a90fc4518ce8767e9df3d6be55c4
Matteo Riondato <riondato@acm.org>
2018-04-16 07:37:15 -0400

    Add some links.

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 				<p>I am a researcher in computer science, currently with the
 				Labs group at <a href="http://www.twosigma.com/">Two Sigma</a>, a highly
 				innovative investment firm in New York City. I also have an appointment
-				as Visiting Assistant Professor of Computer Science at Brown
-				University.</p>
+				as Visiting Assistant Professor of <a
+				href="http://cs.brown.edu">Computer Science at Brown
+				University</a>.</p>
 				<p>My research interest is in <em>algorithmic data science</em>. I
 develop
 				theory and methods to extract the most information from large datasets,
@@ -12,27 +13,37 @@ develop
 				study include pattern extraction, graph mining, and time series
 				analysis. My algorithms often use concepts from statistical learning
 				theory and sampling.</p>
-				<p>My Erd&#337;s number is 3 (Erd&#337;s &rarr; Suen &rarr; Upfal
-				&rarr; Matteo), and I am a <a
+				<p>My <a
+				href="https://en.wikipedia.org/wiki/Erd%C5%91s_number">Erd&#337;s
+				number is 3</a> (<a
+				href="https://en.wikipedia.org/wiki/Paul_Erd%C5%91s">Erd&#337;s/a>
+				&rarr; <a href="http://math.usf.edu/faculty/ssuen/">Suen</a> &rarr; <a
+				href="https://cs.brown.edu/people/eli">Upfal</a> &rarr; Matteo), and I
+				am a <a
 				href="http://genealogy.math.ndsu.nodak.edu/id.php?id=190626">mathematical
-				descendant</a> of <a href="http://www.cs.brown.edu/people/eli/">Eli
-				Upfal</a>, Eli Shamir (2<sup>nd</sup> generation), Jacques Hadamard
-				(5<sup>th</sup>), Sim&eacute;on Denis Poisson (9<sup>th</sup>), and
-				Pierre-Simon Laplace (10<sup>th</sup>).</p>
+				descendant</a> of <a href="https://cs.brown.edu/people/eli/">Eli
+				Upfal</a>, <a href="https://en.wikipedia.org/wiki/Eli_Shamir">Eli
+				Shamir</a> (2<sup>nd</sup> generation), <a
+				href="https://en.wikipedia.org/wiki/Jacques_Hadamard">Jacques
+				Hadamard</a> (5<sup>th</sup>), <a
+				href="https://en.wikipedia.org/wiki/Sim%C3%A9on_Denis_Poisson">Sim&eacute;on
+				Denis Poisson</a> (9<sup>th</sup>), and <a
+				href="https://en.wikipedia.org/wiki/Pierre-Simon_Laplace">Pierre-Simon
+				Laplace</a> (10<sup>th</sup>).</p>
 				<h2>News</h2>
 				<ul>
 					<li><strong>ACM TKDD:</strong> <a
 					href="papers/RiondatoUpfal-BetweennessRademacherAverages-TKDD.pdf">ABRA</a>,
-					our work (with <a href="http://cs.brown.edu/people/eli">Eli</a>) on
+					our work (with <a href="https://cs.brown.edu/people/eli">Eli</a>) on
 					approximating betweenness centrality with Rademacher averages was
-					accepted for publication in <a href="http://tkdd.acm.org">ACM
+					accepted for publication in <a href="https://tkdd.acm.org">ACM
 					TKDD</a>.</li>
 					<li><strong>FouLarD'18:</strong> I'm co-organizing <a
 					href="foulard18/">FouLarD'18</a>, a workshop on the foundations of
 					learning from data, together with <a
 					href="http://cs.nyu.edu/~mohri">Mehryar Mohri</a>, <a
 					href="https://sites.google.com/a/di.uniroma1.it/panco/">Alessandro
-					Panconesi</a>, and <a href="http://cs.brown.edu/~eli">Eli Upfal</a>.
+					Panconesi</a>, and <a href="https://cs.brown.edu/~eli">Eli Upfal</a>.
 					It will take place in Bertinoro, Italy, in September 2018.</li>
 					<li><strong>IEEE ICDM'18:</strong> I'm serving on the PC of <a
 					href="http://icdm2018.org/">IEEE ICDM'18</a>, which will be in
