e9b29192b237ea9f045a6776e603f0d6492bb5a4
Matteo Riondato <riondato@acm.org>
2018-12-31 14:19:15 -0500

    Add a bio page for my plaintext short bio.

---
+++ bio.php
@@ -0,0 +1,8 @@
+<?php include("header.html"); ?>
+<!-- vim: set ts=2 sw=2 tw=79: -->
+		<h2>Bio</h2>
+		<p>A third-person bio for events:</p>
+		<p><?php include("cv/MatteoRiondato-bio.txt"); ?></p>
+		<p>More information are available in my <a
+		href="cv/MatteoRiondato-CV.pdf">CV</a>.</p>
+<?php include("footer.html"); ?>
