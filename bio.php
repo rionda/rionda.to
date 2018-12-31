465c10b5d5aae9c44f1d2ccc4ddb839f660a40ec
Matteo Riondato <riondato@acm.org>
2018-12-31 14:20:14 -0500

    Fix verb.

--- bio.php
+++ bio.php
@@ -3,6 +3,6 @@
 		<h2>Bio</h2>
 		<p>A third-person bio for events:</p>
 		<p><?php include("cv/MatteoRiondato-bio.txt"); ?></p>
-		<p>More information are available in my <a
+		<p>More information is available in my <a
 		href="cv/MatteoRiondato-CV.pdf">CV</a>.</p>
 <?php include("footer.html"); ?>
