c1e35cbed3b82690afb146e4d2636d964961c695
Matteo Riondato <riondato@acm.org>
2018-12-31 14:49:00 -0500

    Fix title and add some text.

--- bio.php
+++ bio.php
@@ -1,7 +1,7 @@
 <?php include("header.html"); ?>
 <!-- vim: set ts=2 sw=2 tw=79: -->
-		<h2>Bio</h2>
-		<p>A third-person bio for events:</p>
+		<h2>Short Bio</h2>
+		<p>A third-person, one-paragraph bio for events:</p>
 		<p><?php include("cv/MatteoRiondato-bio.txt"); ?></p>
 		<p>More information is available in my <a
 		href="cv/MatteoRiondato-CV.pdf">CV</a>.</p>
