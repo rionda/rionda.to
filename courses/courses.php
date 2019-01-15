6ecd1a7569bbef3f40e7b52e4de05433274a377a
Matteo Riondato <riondato@acm.org>
2019-01-15 10:07:40 -0500

    Add courses page.

---
+++ courses/courses.php
@@ -0,0 +1,9 @@
+<?php include("../header.html"); ?>
+<!-- vim: set ts=2 sw=2 tw=79: -->
+<h2>Teaching</h2>
+<p>At Amherst College, I am teaching: <a href="cosc-254-s19/">Data Mining</a>
+and <a href="cosc-111-s19/">Intro to CS 1</a>.<p>
+
+<p>At Brown, I used to teach Optimization Methods in Finance.</p>
+
+<?php include("../footer.html"); ?>
