c98db3b390c6d86f9bf86f288feab7aa50545ed4
Matteo Riondato <riondato@acm.org>
2019-01-15 21:24:46 -0500

    Use the document root to specify paths in php.

--- courses/courses.php
+++ courses/courses.php
@@ -1,4 +1,4 @@
-<?php include("../header.html"); ?>
+<?php include($_SERVER["DOCUMENT_ROOT"].'/header.html'); ?>
 <!-- vim: set ts=2 sw=2 tw=79: -->
 <h2>Teaching</h2>
 <p>At Amherst College, I am teaching: <a href="cosc-254-s19/">Data Mining</a>
@@ -6,4 +6,4 @@ and <a href="cosc-111-s19/">Intro to CS 1</a>.<p>
 
 <p>At Brown, I used to teach Optimization Methods in Finance.</p>
 
-<?php include("../footer.html"); ?>
+<?php include($_SERVER["DOCUMENT_ROOT"].'/footer.html'); ?>
