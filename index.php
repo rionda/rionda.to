a9f89cffe0473f695a4af13464943ce81bb4a29b
Matteo Riondato <riondato@acm.org>
2019-01-24 09:01:49 -0500

    Move news to its own div, to achieve the effect that we want.

--- index.php
+++ index.php
@@ -1,72 +1,76 @@
 <?php include("header.html"); ?>
 				<!-- vim: set ts=2 sw=2 tw=79: -->
-				<div>
-				<img src="/files/matteo_riondato.jpg" alt="Head shot of Matteo Riondato
-				by Andrea Podest&agrave;" title="Matteo Riondato"
-				style="float:left;padding:0 0.5em 0.5em 0;"
-				/>
-				<p>I am an <strong>assistant professor</strong> of <a
-				href="https://www.amherst.edu/academiclife/departments/computer_science">computer
-				science</a> at <a href="https://www.amherst.edu">Amherst
-				College</a>. I also have an appointment as visiting faculty in <a
-				href="https://cs.brown.edu">Computer Science at Brown
-				University</a>. Previously, I spent some fantastic years as a research
-				scientist in the Labs group at <a href="https://www.twosigma.com/">Two
-				Sigma</a>.</p>
-				<p>My research focuses on <strong>algorithms</strong> for
-				<strong>knowledge discovery</strong>, <strong>data mining</strong>, and
-				<strong>machine learning</strong>. I develop theory and methods to
-				extract the most information from large datasets, as fast as possible
-				and in a statistically sound way. The problems I study include pattern
-				extraction, graph mining, and time series analysis. My algorithms often
-				use concepts from statistical learning theory and sampling.</p>
-				<p>My <a
-				href="https://en.wikipedia.org/wiki/Erd%C5%91s_number">Erd&#337;s
-				number</a> is 3 (<a
-				href="https://en.wikipedia.org/wiki/Paul_Erd%C5%91s">Erd&#337;s</a>
-				&rarr; <a href="http://math.usf.edu/faculty/ssuen/">Suen</a> &rarr; <a
-				href="https://cs.brown.edu/people/eli">Upfal</a> &rarr; Matteo), and I
-				am a <a
-				href="https://genealogy.math.ndsu.nodak.edu/id.php?id=190626">mathematical
-				descendant</a> of <a href="https://cs.brown.edu/people/eli/">Eli
-				Upfal</a>, <a href="https://en.wikipedia.org/wiki/Eli_Shamir">Eli
-				Shamir</a> (2<sup>nd</sup> generation), <a
-				href="https://en.wikipedia.org/wiki/Jacques_Hadamard">Jacques
-				Hadamard</a> (5<sup>th</sup>), <a
-				href="https://en.wikipedia.org/wiki/Sim%C3%A9on_Denis_Poisson">Sim&eacute;on
-				Denis Poisson</a> (9<sup>th</sup>), and <a
-				href="https://en.wikipedia.org/wiki/Pierre-Simon_Laplace">Pierre-Simon
-				Laplace</a> (10<sup>th</sup>).</p>
+				<div style="clear:both;">
+					<img src="/files/matteo_riondato.jpg" alt="Head shot of Matteo
+					Riondato by Andrea Podest&agrave;" title="Matteo Riondato"
+					style="float:left;padding:0 0.5em 0.5em 0;" />
+					<p>I am an <strong>assistant professor</strong> of <a
+					href="https://www.amherst.edu/academiclife/departments/computer_science">computer
+					science</a> at <a href="https://www.amherst.edu">Amherst
+					College</a>. I also have an appointment as visiting faculty in <a
+					href="https://cs.brown.edu">Computer Science at Brown
+					University</a>. Previously, I spent some fantastic years as a
+					research scientist in the Labs group at <a
+					href="https://www.twosigma.com/">Two Sigma</a>.</p>
+					<p>My research focuses on <strong>algorithms</strong> for
+					<strong>knowledge discovery</strong>, <strong>data mining</strong>,
+					and <strong>machine learning</strong>. I develop theory and methods
+					to extract the most information from large datasets, as fast as
+					possible and in a statistically sound way. The problems I study
+					include pattern extraction, graph mining, and time series analysis.
+					My algorithms often use concepts from statistical learning theory and
+					sampling.</p>
+					<p>My <a
+					href="https://en.wikipedia.org/wiki/Erd%C5%91s_number">Erd&#337;s
+					number</a> is 3 (<a
+					href="https://en.wikipedia.org/wiki/Paul_Erd%C5%91s">Erd&#337;s</a>
+					&rarr; <a href="http://math.usf.edu/faculty/ssuen/">Suen</a> &rarr;
+					<a href="https://cs.brown.edu/people/eli">Upfal</a> &rarr; Matteo),
+					and I am a <a
+					href="https://genealogy.math.ndsu.nodak.edu/id.php?id=190626">mathematical
+					descendant</a> of <a href="https://cs.brown.edu/people/eli/">Eli
+					Upfal</a>, <a href="https://en.wikipedia.org/wiki/Eli_Shamir">Eli
+					Shamir</a> (2<sup>nd</sup> generation), <a
+					href="https://en.wikipedia.org/wiki/Jacques_Hadamard">Jacques
+					Hadamard</a> (5<sup>th</sup>), <a
+					href="https://en.wikipedia.org/wiki/Sim%C3%A9on_Denis_Poisson">Sim&eacute;on
+					Denis Poisson</a> (9<sup>th</sup>), and <a
+					href="https://en.wikipedia.org/wiki/Pierre-Simon_Laplace">Pierre-Simon
+					Laplace</a> (10<sup>th</sup>).</p>
 				</div>
-				<h2>News</h2>
-				<ul>
-					<li><strong>Teaching:</strong> in my first semester at Amherst
-					College, I'm teaching <a href="courses/cosc-254-s19/">Data Mining</a>
-					and <a href="courses/cosc-111-s19/">Intro to CS 1</a>. Thrilled to be
-					working closely with brilliant students again.</li>
-					<li><strong>KAIS:</strong> an extended version of <a
-					href="papers/ServanSchreiberEtAl-ProSecCo-KAIS.pdf">ProSecCo:
-					Progressive Sequence Mining with Convergence Guarantees</a> was
-					invited to the special issue of <a
-					href="https://link.springer.com/journal/10115">KAIS</a> for the best
-					papers of <a href="http://www.icdm18.org">IEEE ICDM'18</a>.</li>
-					<li><strong>PCs and editorial board:</strong> glad to serve on the PC
-					for <a href="https://icml.cc/Conferences/2019">ICML'19</a>, <a
-					href="https://www.kdd.org/kdd2019/">ACM KDD'19</a>, <a
-					href="http://vermontcomplexsystems.org/events/netsci/">NetSci'19</a>,
-					and <a href="https://www.ijcai19.org/">IJCAI'19</a>, and on the
-					guest editorial board of the Data Mining and Knowledge Discovery
-					journal for <a href="https://ecmlpkdd2019.org/">ECML
-					PKDD'19</a>.</li>
-					<li><strong>ACM TKDD:</strong> an extended version of <a
-					href="papers/RiondatoVandin-MiSoSouPExtended-KDD.pdf">MiSoSouP</a>
-					was invited to the special issue of <a
-					href="https://tkdd.acm.org">ACM TKDD</a> for the best papers of <a
-					href="https://www.kdd.org/kdd2018/">KDD'18</a>.</li>
-					<li><strong>SDM'19:</strong> I'm serving as co-chair of the Doctoral
-					Forum for <a href="https://www.siam.org/meetings/sdm19/">SIAM
-					SDM'19</a>. Very excited to help with the organization, as I won the
-					best student poster award at this event in 2014.</li>
-					<li><a href="oldnews.php">News archive</a></li>
-				</ul>
+				<div style="clear:both;">
+					<h2>News</h2>
+					<ul>
+						<li><strong>Teaching:</strong> in my first semester at Amherst
+						College, I'm teaching <a href="courses/cosc-254-s19/">Data
+						Mining</a> and <a href="courses/cosc-111-s19/">Intro to CS 1</a>.
+						Thrilled to be working closely with brilliant students again.</li>
+						<li><strong>KAIS:</strong> an extended version of <a
+						href="papers/ServanSchreiberEtAl-ProSecCo-KAIS.pdf">ProSecCo:
+						Progressive Sequence Mining with Convergence Guarantees</a> was
+						invited to the special issue of <a
+						href="https://link.springer.com/journal/10115">KAIS</a> for the
+						best papers of <a href="http://www.icdm18.org">IEEE
+						ICDM'18</a>.</li>
+						<li><strong>PCs and editorial board:</strong> glad to serve on the
+						PC for <a href="https://icml.cc/Conferences/2019">ICML'19</a>, <a
+						href="https://www.kdd.org/kdd2019/">ACM KDD'19</a>, <a
+						href="http://vermontcomplexsystems.org/events/netsci/">NetSci'19</a>,
+						and <a href="https://www.ijcai19.org/">IJCAI'19</a>, and on the
+						guest editorial board of the Data Mining and Knowledge Discovery
+						journal for <a href="https://ecmlpkdd2019.org/">ECML
+						PKDD'19</a>.</li>
+						<li><strong>ACM TKDD:</strong> an extended version of <a
+						href="papers/RiondatoVandin-MiSoSouPExtended-KDD.pdf">MiSoSouP</a>
+						was invited to the special issue of <a
+						href="https://tkdd.acm.org">ACM TKDD</a> for the best papers of <a
+						href="https://www.kdd.org/kdd2018/">KDD'18</a>.</li>
+						<li><strong>SDM'19:</strong> I'm serving as co-chair of the
+						Doctoral Forum for <a
+						href="https://www.siam.org/meetings/sdm19/">SIAM SDM'19</a>. Very
+						excited to help with the organization, as I won the best student
+						poster award at this event in 2014.</li>
+						<li><a href="oldnews.php">News archive</a></li>
+					</ul>
+				</div>
 <?php include("footer.html"); ?>
