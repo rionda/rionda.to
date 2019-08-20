678adc55cc7155e1e659890ff30acc382807b8bb
Matteo Riondato <riondato@acm.org>
2019-08-20 10:57:37 -0400

    Change the courses page, adding syllabuses for old courses.

--- courses/courses.php
+++ courses/courses.php
@@ -1,8 +1,14 @@
 <?php include($_SERVER["DOCUMENT_ROOT"].'/header.html'); ?>
 <!-- vim: set ts=2 sw=2 tw=79: -->
 <h2>Teaching</h2>
-<p>At Amherst College, I am teaching: <a href="cosc-254-s19/">Data Mining</a>
-and <a href="cosc-111-s19/">Intro to CS 1</a>.<p>
+<p>At Amherst College, I am teaching:</p>
+<ul>
+	<li>COSC-257 Databases (Fall'19)</li>
+	<li>COSC-254 Data Mining
+		(<a href="datamining-syllabus-s19.pdf">Spring'19</a>)</li>
+	<li>COSC-111 Intro to CS 1 (Fall'19, <a
+href="introcs1-syllabus-s19.pdf">Spring'19</a>).</li>
+</ul>
 
 <p>At Brown, I used to teach Optimization Methods in Finance.</p>
 
