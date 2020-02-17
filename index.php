d55ebafd33474ddcee3c3a1dc85b8c3fa9be826d
Matteo Riondato <riondato@acm.org>
2020-02-16 19:20:25 -0500

    Some news.

--- index.php
+++ index.php
@@ -41,31 +41,14 @@
 				<div id="news" style="clear:both;">
 					<h2>News</h2>
 					<ul>
-						<li><strong>NetSciI@NEU (11/8):</strong> I'll be giving a talk at
-the Network Science Institute at Northeastern University. Thank you <a
-href="http://www.eliassi.org">Tina</a> for inviting me: the place and people
-there are wonderful and do super cool work.</li>
-						<li><strong>MassMutual Research Bytes(11/5):</strong> I'll be
-giving a talk on making better use of data through hypothesis testing and
-statistical learning theory. Thank you <a href="https://www.amherst.edu/people/facstaff/nhorton?shib_redir=108028591">Nick</a> for inviting me.</li>
-						<li><strong>BU (10/25):</strong> I'll be giving a talk at Boston
-University. Thank you <a href="https://www.cs.bu.edu/~evimaria/">Evimaria</a>
-for inviting me.</li>
-						<li><strong>MHC (10/16):</strong> I'll be giving a talk at Mount
-Holyoke College. Thanks to <a
-href="https://www.mtholyoke.edu/~vbarr/">Valerie</a> for inviting me.</a></li>
-						<li><strong>CaStleD'19:</strong> I'm giving a talk on <a
-href="papers/CousinsRiondato-CaDET-MachLearn.pdf">CaDET</a> at <a
-href="https://www.dei.unipd.it/~vandinfa/castled19/">CaStleD'19</a> in <a
-href="http://www.bici.eu">Bertinoro</a>. Thank you <a
-href="http://www.dei.unipd.it/~vandinfa">Fabio</a> for inviting me.</li>
-						<li><strong>ECML PKDD:</strong> <a href="papers/CousinsRiondato-CaDET-MachLearn.pdf">CaDET</a>, our algorithm
-for interpretable conditional density estimation using decision trees has been
-accepted to the special issue of <a
-href="https://www.springer.com/computer/ai/journal/10994">Machine Learning</a>
-for <a href="http://ecmlpkdd2019.org/">ECML PKDD'19</a>. Joint work with
-thinker-extraordinaire PhD student <a
-href="http://cs.brown.edu/~ccousins">Cyrus Cousins</a>.</li>
+						<li><strong>SIAM SDM'20:</strong> We'll present our <a href="statdmtut/">tutorial on
+Statistical Hypothesis Testing and Pattern Mining</a> at<a
+href="https://www.siam.org/conferences/cm/conference/sdm20">SIAM SDM'20</a>, a
+great event bringing together computer scientists and statisticians.</li>
+						<li><strong>DMKD:</strong> Very honored to become a member of the
+						<a href="https://link.springer.com/journal/10618">Data Mining and
+						Knowledge Discovery</a> journal, one of the top venues in my area
+						of research.</li>
 						<li><a href="oldnews.php">News archive</a></li>
 					</ul>
 				</div>
--- oldnews.php
+++ oldnews.php
@@ -5,6 +5,34 @@
 		<p>A list of old news, moved here from the home page, mostly for my
 		personal records.</p>
 		<ul>
+			<li><strong>NetSciI@NEU (11/8):</strong> I'll be giving a talk at the
+			Network Science Institute at Northeastern University. Thank you <a
+href="http://www.eliassi.org">Tina</a> for inviting me: the place and people
+			there are wonderful and do super cool work.</li>
+			<li><strong>MassMutual Research Bytes(11/5):</strong> I'll be giving a
+			talk on making better use of data through hypothesis testing and
+			statistical learning theory. Thank you <a
+href="https://www.amherst.edu/people/facstaff/nhorton?shib_redir=108028591">Nick</a>
+			for inviting me.</li>
+			<li><strong>BU (10/25):</strong> I'll be giving a talk at Boston
+			University. Thank you <a href="https://www.cs.bu.edu/~evimaria/">Evimaria</a>
+for inviting me.</li>
+			<li><strong>MHC (10/16):</strong> I'll be giving a talk at Mount
+			Holyoke College. Thanks to <a
+			href="https://www.mtholyoke.edu/~vbarr/">Valerie</a> for inviting
+			me.</a></li>
+			<li><strong>CaStleD'19:</strong> I'm giving a talk on <a
+			href="papers/CousinsRiondato-CaDET-MachLearn.pdf">CaDET</a> at <a
+			href="https://www.dei.unipd.it/~vandinfa/castled19/">CaStleD'19</a> in <a
+			href="http://www.bici.eu">Bertinoro</a>. Thank you <a
+			href="http://www.dei.unipd.it/~vandinfa">Fabio</a> for inviting me.</li>
+			<li><strong>ECML PKDD:</strong> <a href="papers/CousinsRiondato-CaDET-MachLearn.pdf">CaDET</a>, our algorithm
+			for interpretable conditional density estimation using decision trees has been
+			accepted to the special issue of <a
+			href="https://www.springer.com/computer/ai/journal/10994">Machine Learning</a>
+			for <a href="http://ecmlpkdd2019.org/">ECML PKDD'19</a>. Joint work with
+			thinker-extraordinaire PhD student <a
+			href="http://cs.brown.edu/~ccousins">Cyrus Cousins</a>.</li>
 			<li><strong>GPG key:</strong> I updated <a
 href="files/riondapgp.asc">my GPG key</a>, which had expired. The keys did not
 change, only the expiration date.</li>
