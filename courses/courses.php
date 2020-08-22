30191342860a11c0c628e302eb76fc859a51dbac
Matteo Riondato <riondato@acm.org>
2020-08-22 15:28:17 -0400

    Revamp the teaching page.

--- courses/courses.php
+++ courses/courses.php
@@ -1,15 +1,18 @@
 <?php include($_SERVER["DOCUMENT_ROOT"].'/header.html'); ?>
 <!-- vim: set ts=2 sw=2 tw=79: -->
 <h2>Teaching</h2>
-<p>At Amherst College, I am teaching:</p>
+<p>At Amherst College, I teach:</p>
 <ul>
+	<li>COSC-254 Data Mining (Spring'19, Spring'21)</li>
 	<li>COSC-257 Databases (Fall'19)</li>
-	<li>COSC-254 Data Mining
-		(<a href="datamining-syllabus-s19.pdf">Spring'19</a>)</li>
-	<li>COSC-111 Intro to CS 1 (Fall'19, <a
-href="introcs1-syllabus-s19.pdf">Spring'19</a>).</li>
+	<li>COSC-355 Network Science (Spring'20)</li>
+	<li>COSC-111 Intro to CS 1 (Spring'19, Fall'19, Spring'20, Fall'20 (x2),
+	Spring'21)</li>
+	<li>The occasional (but frequent&hellip;) <em>independent study</em> course
+	on machine learning, data mining, or other topics of interest</li>
 </ul>
 
-<p>At Brown, I used to teach Optimization Methods in Finance.</p>
+<p>At Brown, I taught Optimization Methods in Finance in Spring'16 and
+Spring'18.</p>
 
 <?php include($_SERVER["DOCUMENT_ROOT"].'/footer.html'); ?>
