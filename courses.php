fb3a15d5d6689d19a002c006bde40c7ba1ef2a70
Matteo Riondato <riondato@acm.org>
2021-10-17 09:55:04 -0400

    Expand a bit on the courses.

--- courses.php
+++ courses.php
@@ -1,16 +1,27 @@
 <?php include($_SERVER["DOCUMENT_ROOT"].'/header.html'); ?>
 <!-- vim: set ts=2 sw=2 tw=79: -->
 <h2>Teaching</h2>
-<p>At Amherst College, I teach:</p>
+<p>At Amherst College, I regularly teach:</p>
 <ul>
 	<li>COSC-254 Data Mining (Spring'19, Spring'21)</li>
 	<li>COSC-257 Databases (Fall'19)</li>
 	<li>COSC-355 Network Science (Spring'20)</li>
 	<li>COSC-111 Intro to CS 1 (Spring'19, Fall'19, Spring'20, Fall'20 (x2),
 	Spring'21)</li>
-	<li>The occasional (but frequent&hellip;) <em>independent study</em> course
-	on machine learning, data mining, or other topics of interest</li>
 </ul>
+
+<p>I also often supervise <em>independent study</em> courses on machine
+learning, data mining, or other topics of interet, for example:</p>
+<ul>
+	<li>Topics in Data Science</li>
+	<li>Database Implementation</li>
+	<li>Advanced Analytics</li>
+	<li>Regression and Time Series Analysis</li>
+</ul>
+
+<p>In Fall'19 and Fall'20 I also taught "COLQ-390H Learning by Doing:
+Internship and Fieldwork Reflection", the half-credit course to satisfy the
+CPT requirement for international students who did an internship.</p>
 
 <p>At Brown, I taught Optimization Methods in Finance in Spring'16 and
 Spring'18.</p>
