e997b3c38fbc7e8565830091968fd394b65be51b
Matteo Riondato <rionda@acm.org>
2022-12-15 16:30:51 -0500

    Add Bavarian DOI.

--- pubs.php
+++ pubs.php
@@ -36,7 +36,8 @@
 						style="font-variant:small-caps">Bavarian</span>: Betweenness
 						Centrality Approximation with Variance-Aware Rademacher
 						Averages</a>, <em>ACM Transactions on Knowledge Discovery
-						from Data</em>, to appear.</li>
+						from Data</em>, DOI: <a
+						href="https://doi.org/10.1145/3577021">10.1145/3577021</a>.</li>
 					<li>S. Haddadan, C. Menghini, M. Riondato, and E. Upfal. <a
 						href="papers/HaddadanEtAl-RePBubLik-DMKD.pdf">Reducing
 						Polarization and Increasing Diverse Navigability in Graph by
