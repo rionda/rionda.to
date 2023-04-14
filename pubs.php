dd426167bb65ca6d3487e5e4a1874fbca909bc88
Matteo Riondato <rionda@acm.org>
2023-04-14 10:50:23 -0400

    Add Maniacs TIST infos

--- pubs.php
+++ pubs.php
@@ -36,7 +36,7 @@
 						style="font-variant:small-caps">MaNIACS</span>: Approximate Mining
 						of Frequent Subgraph Patterns through Sampling</a>, <em>ACM
 						Transactions on Intelligent Systems and Technology</em>,
-						DOI:10.1145/3587254, to appear.</li>
+						14(3):54:1</li>
 					<li>C. Cousins, C. Wohlgemuth, and M. Riondato. <a
 						href="papers/CousinsEtAl-Bavarian-TKDD23.pdf"><span
 						style="font-variant:small-caps">Bavarian</span>: Betweenness
