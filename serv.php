4c9f7ef790fd214ef05e8e1d5c2a14349699aa21
Matteo Riondato <rionda@acm.org>
2023-08-23 17:38:33 -0400

    serv: remove PC/journal list, add OCs.

--- serv.php
+++ serv.php
@@ -6,6 +6,7 @@
 		<p class="notop">Machine Learning (guest editorial board for ECML
 		PKDD'19&mdash;23)</p>
 		<h3>Organizing Committees</h3>
+		<p class="notop">2024: SIAM SDM'24 (Blue-sky-idea Track)</p>
 		<p class="notop">2023: ACM MAGICC (Keynote and Panel), ECML PKDD'23
 			(Doctoral Forum Co-Chair)</p>
 		<p class="notop">2022: SIAM SDM'22 (PC Co-Chair)</p>
@@ -16,74 +17,11 @@
 		href="foulard18/">Foundations on Learning from Data Workshop</a>
 		(Organizer)</p>
 		<h3>Program Committees</h3>
-		<p class="notop">2023: ACM KDD'23 (Senior PC), WWW'23 (Senior PC), IEEE
-			ICDM'23, AAAI'23 (Senior PC), ACM WSDM'23 (Senior PC), SIAM SDM'23
-			(Senior PC)</p>
-		<p class="notop">2022: ACM KDD'22 (Senior PC), ACM WSDM'22 (Senior PC),
-			WWW'22 (Senior PC), ICDM'21, ECML PKDD'22, NeurIPS'22</p>
-		<p class="notop">2021: ACM CIKM'21, ACM KDD'21 (Senior PC), ACM WSDM'21,
-			ECML PKDD'21, ICDM'21 (Area Chair), NeurIPS'21, SIAM SDM'21 (Senior PC),
-			WWW'21 (Senior PC)</p>
-		<p class="notop">2020: AAAI'20, ACM CIKM'20, ACM KDD'20, ECML PKDD'20,
-		ICDM'20, NeurIPS'20, SIAM SDM'20, ACM WSDM'20, WWW'20</p>
-		<p class="notop">2019: AAAI'19, ACM CIKM'19, ACM WSDM'19, ACM KDD'19, ECML
-			PKDD'19, ICDM'19, ICML'19, IJCAI'19, NetSci'19, NeurIPS'19, SIAM SDM'19,
-			WWW'19</p>
-		<p class="notop">2018: IEEE ICDM'18, ACM KDD'18, WWW'18, IEEE ICDE'18,
-			Grace Hopper'18, ECML PKDD'18, ACM CIKM'18</p>
-		<p class="notop">2017: ACM KDD'17 (Senior PC), ACM WSDM'17, WWW'17, IEEE
-			ICDE'17, ECML PKDD '17, ACM CIKM'17</p>
-		<p class="notop">2016: IEEE ICDM'16, ACM CIKM'16, Grace Hopper'16, ECML
-			PKDD'16, ACM KDD'16, WWW'16</p>
-		<p class="notop">2015: ACM KDD'15, ACM CIKM'15</p>
-		<p class="notop">2014: ACM CIKM'14</p>
+		<p>I regularly serve on the PC, often as Senior PC / Area Chair, of many
+		prominent Data Mining / Machine Learning conferences, such as ACM KDD, WWW,
+		IEEE ICDM, NeurIPS, AAAI, ACM WSDM, SIAM SDM, ECML PKDD, and others.</p>
 		<h3>Journal Reviewing</h3>
-		<p class="notop"><em>ACM Transactions on Database Systems</em> (TODS)</p>
-		<p class="notop"><em>ACM Transactions on Information Systems</em> (TOIS)</p>
-		<p class="notop"><em>ACM Transactions on Knowledge Discovery from Data</em> (TKDD)</p>
-		<p class="notop"><em>Algorithmica</em></p>
-		<p class="notop"><em>Data Mining and Knowledge Discovery</em> (DMKD/DAMI)</p>
-		<p class="notop"><em>Discrete Applied Mathematics</em> (DAM)</p>
-		<p class="notop"><em>Engineering Applications of Artificial Intelligence</em> (EAAI)</p>
-		<p class="notop"><em>Expert Systems with Applications</em> (ESWA)</p>
-		<p class="notop"><em>Journal of Parallel and Distributed Computing</em> (JDPC)</p>
-		<p class="notop"><em>Knowledge and Information Systems</em> (KAIS)</p>
-		<p class="notop"><em>IEEE Transactions on Knowledge and Data Engineering</em> (TKDE)</p>
-		<p class="notop"><em>IEEE Transactions on Network Science and Engineering</em> (TNSE)</p>
-		<p class="notop"><em>IEEE Transactions on Parallel and Distributed Systems</em> (TPDS)</p>
-		<p class="notop"><em>IEEE Transactions on Services Computing</em> (TSC)</p>
-		<p class="notop"><em>Information Science</em> (INS)</p>
-		<p class="notop"><em>Machine Learning</em> (Mach. Learn.)</p>
-		<p class="notop"><em>PLOS ONE</em></p>
-		<p class="notop"><em>PeerJ</em></p>
-		<p class="notop"><em>The Computer Journal</em></p>
-		<p class="notop"><em>VLDB Journal</em> (VLDBJ)</p>
-		<h3>Conference Reviewing</h3>
-		<p>(Not including reviews done as Program Committee member)</p>
-		<p class="notop">2018: ESA'18</p>
-		<p class="notop">2017: MFCS'17, SODA'17</p>
-		<p class="notop">2015: ISAAC'15, SIAM SDM'15, IEEE ICDE'15</p>
-		<p class="notop">2014: DISC'14, ACM WSDM'14, WWW'14, ICALP'14</p>
-		<p class="notop">2013: IEEE BigData'13, MFCS'13, ACM WSDM'13</p>
-		<p class="notop">2012: IEEE IPDPS'12, ACM ICS'12</p>
-		<p class="notop">2011: APPROX-RANDOM'11</p>
-		<h3>Conference Session Chairing</h3>
-		<p class="notop">KDD'21: Theory &mdash; Learning and Generalization</p>
-		<p class="notop">SDM'21: Foundations II</p>
-		<p class="notop">ICDM'20: Graph Mining II</p>
-		<p class="notop">KDD'20: Data Mining Methods</p>
-		<p class="notop">WWW'20: Social Network Analysis III</p>
-		<p class="notop">SDM'19: Patterns</p>
-		<p class="notop">KDD'18: Unsupervised Learning II</p>
-		<p class="notop">KDD'17: Graphs 1</p>
-		<p class="notop">KDD'16: Graphs 1</p>
-		<p class="notop">ICDM'16: Theory</p>
-		<p class="notop">ECML PKDD'16: Graphs and Social Networks 1</p>
-		<p class="notop">WWW'16: Social Networks and Graph Analysis 1</p>
-		<p class="notop">KDD'15: Social and Graphs 4</p>
-		<h3>Grant Proposal Reviewing</h3>
-		<p class="notop">NSF SBIR/STTR Program</p>
-		<p class="notop">Grace Hopper Conference Scholarship (2016 and 2017)</p>
-		<p class="notop">Sigma Delta Epsilon (Graduate Women in Science) National
-		Fellowship</p>
+		<p>I constantly review for many Data Mining / Machine Learning journals:
+		ACM TKDD, DAMI/DMKD, IEEE TKDE, JMLR, Mach. Learn, ACM TODS, ACM TIST,
+	  KAIS, VLDBJ, and others.</p>
 <?php include("footer.html"); ?>
