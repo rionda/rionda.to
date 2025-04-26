b5777efa6191232e61b6422fef7391b66650d9f5
Matteo Riondato <rionda@acm.org>
2025-04-26 09:49:19 -0400

    Update courses.

--- courses.php
+++ courses.php
@@ -1,13 +1,15 @@
 <?php include($_SERVER["DOCUMENT_ROOT"].'/header.html'); ?>
 <!-- vim: set ts=2 sw=2 tw=79: -->
 <h2>Teaching</h2>
-<p>At Amherst College, I regularly teach:</p>
+<p>At Amherst College, I teach:</p>
 <ul>
-	<li>COSC-254 Data Mining (Spring'19, Spring'21)</li>
-	<li>COSC-257 Databases (Fall'19, Fall'23)</li>
+	<li>COSC-223 Probability and Computing (Fall'24)</li>
+	<li>COSC-254 Data Mining (Spring'25, Spring'21, Spring'19)</li>
+	<li>COSC-257 Databases (Fall'23, Fall'19)</li>
 	<li>COSC-351 Information Theory (Spring'23)</li>
-	<li>COSC-355 Network Science (Spring'20, Fall'22)</li>
-	<li>COSC-111 Intro to CS 1 (essentially every semester)</li>
+	<li>COSC-355 Network Science (Fall'25, Fall'22, Spring'20)</li>
+  <li>COSC-401 Theoretical Foundations of Computer Science (Spring'25)</li>
+	<li>COSC-111 Intro to CS 1 (almost every semester)</li>
 </ul>
 
 <p>I often supervise <em>independent study</em> courses on machine learning,
@@ -19,8 +21,8 @@ data mining, or other topics of interest, e.g.:</p>
 	<li>Regression and Time Series Analysis</li>
 </ul>
 
-<p>Every Fall, unless I'm on leave, I also teach "COLQ-390H Learning by Doing:
-Internship and Fieldwork Reflection", the half-credit course to satisfy the
+<p>Every Fall, unless I'm on leave, I also teach "COLQ-390U Learning by Doing:
+Internship and Fieldwork Reflection", the quarter-credit course to satisfy the
 CPT requirement for international students who did an internship.</p>
 
 <p>At Brown, I taught Optimization Methods in Finance in Spring'16 and
