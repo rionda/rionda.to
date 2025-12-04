0641a7f7dfc46d2ce6521c891f04e427c3cc8457
Matteo Riondato <rionda@acm.org>
2025-12-04 08:31:20 -0500

    bio: add missing dot.

--- bio.php
+++ bio.php
@@ -5,5 +5,5 @@
 		<p><?php include("cv/MatteoRiondato-bio.txt"); ?></p>
 		<p>More information is available in my <a
 		href="cv/MatteoRiondato-CV.pdf">CV</a>.</p>
-		<p>A <a href="files/matteo_riondato_big.jpg">large headshot</a>, just in case</p>
+		<p>A <a href="files/matteo_riondato_big.jpg">large headshot</a>, just in case.</p>
 <?php include("footer.html"); ?>
