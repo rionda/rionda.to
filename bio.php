da59a0cf18e27835c1949c785c1521b44655e756
Matteo Riondato <rionda@acm.org>
2025-12-04 08:50:15 -0500

    bio: use blockquote for my bio.

--- bio.php
+++ bio.php
@@ -2,7 +2,7 @@
 <!-- vim: set ts=2 sw=2 tw=79: -->
 		<h2>Short Bio</h2>
 		<p>A third-person, one-paragraph bio for events:</p>
-		<p><?php include("cv/MatteoRiondato-bio.txt"); ?></p>
+		<blockquote><?php include("cv/MatteoRiondato-bio.txt"); ?></blockquote>
 		<p>More information is available in my <a
 		href="cv/MatteoRiondato-CV.pdf">CV</a>.</p>
 		<p>A <a href="files/matteo_riondato_big.jpg">large headshot</a>, just in case.</p>
